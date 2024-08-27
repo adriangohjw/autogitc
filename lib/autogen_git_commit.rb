cea7d7595f724430da34a1282df6f5908759e294
adriangohjw <adriangohjw@gmail.com>
2024-08-27 15:54:45 +0800

    refactor: standardize string quotes in autogen_git_commit.rb

--- lib/autogen_git_commit.rb
+++ lib/autogen_git_commit.rb
@@ -16,47 +16,49 @@ module AutogenGitCommit
   end
 
   def self.generate_commit_message(changes_summary)
-    uri = URI("https://api.openai.com/v1/chat/completions")
-    
+    uri = URI('https://api.openai.com/v1/chat/completions')
+
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
-    
-    request = Net::HTTP::Post.new(uri.path, {
-      "Content-Type" => "application/json",
-      "Authorization" => "Bearer #{openai_api_key}"
-    })
-    
+
+    request = Net::HTTP::Post.new(
+      uri.path, {
+        'Content-Type' => 'application/json',
+        'Authorization' => "Bearer #{openai_api_key}"
+      }
+    )
+
     request.body = {
-      model: "gpt-4o-mini",
+      model: 'gpt-4o-mini',
       messages: [
         {
-          role: "system",
-          content: "You are a helpful assistant that generates concise and semantic git commit messages."
+          role: 'system',
+          content: 'You are a helpful assistant that generates concise and semantic git commit messages.'
         },
         {
-          role: "user",
+          role: 'user',
           content: "Generate a conventional git commit message limited to 72 characters for the following changes in a JSON format with a key 'commit_message':\n\n#{changes_summary}"
         }
       ],
       response_format: { "type": 'json_object' }
     }.to_json
-    
+
     response = http.request(request)
     result = JSON.parse(response.body)
-    output_text = result.dig("choices", 0, "message", "content")
-    JSON.parse(output_text).dig('commit_message')
+    output_text = result.dig('choices', 0, 'message', 'content')
+    JSON.parse(output_text)['commit_message']
   end
 
   def self.main
     if openai_api_key.nil?
-      puts "No OPENAI_API_KEY key found in environment variables."
+      puts 'No OPENAI_API_KEY key found in environment variables.'
       return
     end
 
     staged_files = get_staged_files
 
     if staged_files.empty?
-      puts "No files are staged for commit."
+      puts 'No files are staged for commit.'
       return
     end
 
